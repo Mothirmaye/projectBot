0a37df91b527aef4d032ffa50ad83b2787fc83bc
vlab <vlab>
2016-10-24 18:25:00 +0530

    created app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ server.listen(process.env.PORT || 3000, function()
 
 // Create chat bot
 var connector = new builder.ChatConnector
-({ appId: 'projectBot', appPassword: 'mothi1130' }); 
+({ appId: 'mothirmaye@gmail.com', appPassword: 'mothi1130' }); 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
